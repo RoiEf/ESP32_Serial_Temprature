5fd56f092420f50c7edf7b08c618a507b219aedf
RoiEf <roi.efrati@gmail.com>
2020-09-30 22:41:50 +0300

    Test success

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
+#include <Adafruit_MLX90614.h>
 #include <Arduino.h>
+#include <Wire.h>
 
+#define BAUD 115200
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(BAUD);
+    mlx.begin();
+    delay(100);
+
+    Serial.println("ESP32 MLX90614");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // Serial.print("Ambient = ");
+    // Serial.print(mlx.readAmbientTempC());
+    // Serial.print("*C\tObject = ");
+    // Serial.print(mlx.readObjectTempC());
+    // Serial.println("*C");
+
+    // Serial.println();
+    delay(50);
 }
